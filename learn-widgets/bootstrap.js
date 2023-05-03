7d67f7c6dcabd1a8d8800b2d705c03d9a51802e3
Lorena Kell <lorena.kell@yahoo.com>
2023-05-03 16:05:26 -0500

    completed widget

--- learn-widgets/bootstrap.js
+++ learn-widgets/bootstrap.js
@@ -1,8 +1,8 @@
-document.write(‘<link rel="stylesheet" type="text/css" href="style.css">’);
+document.write('<link rel="stylesheet" type="text/css" href="style.css">');
 document.write('<script type="text/javascript" src="WidgetCreate.js"></script>');
 
-var myElement = document.getElementById(‘myFirstWidget’);
+var myElement = document.getElementById('myFirstWidget');
 var JavaScriptCode = document.createElement("script");
-JavaScriptCode.setAttribute(‘type’, ‘text/javascript’);
-JavaScriptCode.setAttribute("src", ‘data.js’);
-document.getElementById(‘myFirstWidget’).appendChild(JavaScriptCode);
+JavaScriptCode.setAttribute('type', 'text/javascript');
+JavaScriptCode.setAttribute("src", 'data.js');
+document.getElementById('myFirstWidget').appendChild(JavaScriptCode);
